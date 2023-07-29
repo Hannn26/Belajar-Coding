a391fc0eff3952f1a112d171f7f675096c08ecb4
Han <laxus748799@gmail.com>
2023-07-29 22:15:24 +0700

    Upload Pertama Kali

---
+++ dasar1.js
@@ -0,0 +1,52 @@
+//Create Variable
+let a = 1;
+var b = "b";
+const c = "c";
+
+//Data type
+let Name = "Hann"; //String
+let Age = 10; //Integer
+let Decimal = 1.1; //Float
+let isTrue = true; //Boolean
+
+//Operators
+let Num1 = 10;
+let Num2 = 5;
+console.log(Num1 + Num2, "Addition");
+console.log(Num1 - Num2, "Subtraction");
+console.log(Num1 / Num2, "Division");
+console.log(Num1 % Num2, "Modulus");
+console.log(Num1 * Num2, "Multiplication");
+
+//Assign Variable
+let hasilPenjumlahan = Num1 + Num2;
+console.log(hasilPenjumlahan);
+
+let hasilPengurangan= Num1 - Num2;
+console.log(hasilPengurangan);
+
+let hasilPembagian = Num1 / Num2;
+console.log(hasilPembagian);
+
+let hasilPerkalian = Num1 * Num2;
+console.log(hasilPerkalian);
+
+let hasilModulus = Num1 % Num2;
+console.log(hasilModulus);
+
+//Operators String
+let firstName = "Han";
+let lastName = "reiz";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName)
+console.log(firstName + " " + lastName)
+
+let Number = 20;
+console.log(Number + 10);
+Number = 10;
+console.log(Number + 10);
+Number = false;
+console.log(Number + 10);
+
+console.log(true + 10);
